e5ef54988112ba394b20195268b2419af99f6226
Andrew <alam1997@gmail.com>
2022-12-20 00:15:06 -0800

    Reduced API calls by storing local priceMap

--- commands/honing_commands.py
+++ commands/honing_commands.py
@@ -1,8 +1,16 @@
 import discord
 
+from util import constants
 from util.embeds import create_embed
 from util.honing import honing_calculator, honing_plotter
+from util.lookup import lookup_handler
 
+priceMap = {}
+for i in constants.SUPPORTED_ITEMS:
+    if i in ["destruction", "guardian", "obliteration", "protection"]:
+        priceMap[i] = lookup_handler.get_item_data(i).json()[0]['avgPrice'] / 10
+    else:
+        priceMap[i] = lookup_handler.get_item_data(i).json()[0]['avgPrice']
 
 async def graph_hones(message):
     honing_plotter.plot_honing_historic(honing_calculator.calculate_honing_historic(message))
